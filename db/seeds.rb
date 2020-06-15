6f17843d3150432dc9928a699bfd492f1a46abed
Bin Le <bin.le.code@gmail.com>
2020-06-15 17:27:18 -0400

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+                        {name: 'Test user 1'},
+                        {name: 'Test user 2', email: 'test_user@testmicropost.com'}
+])
